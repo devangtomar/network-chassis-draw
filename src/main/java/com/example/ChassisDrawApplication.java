129c51b329596ff03b6470fb063565ecac4d36d9
d0t077o <devangtomar123@gmail.com>
2020-03-17 20:25:13 +0530

    modifying  the file ChassisDrawApplication.java

---
+++ src/main/java/com/example/ChassisDrawApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChassisDrawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChassisDrawApplication.class, args);
+	}
+}
